589a74a213e07d47172a4c7a39a600a450122fec
salaudeen <olayiwolasalaudeen@gmail.com>
2024-03-28 12:31:29 +0100

    recaping on javascript by bulding a weather App

---
+++ script/forecast.js
@@ -0,0 +1,23 @@
+const key = "wLsLAgBpsYPAu0qOiE67dTGyAW4OREKe";
+
+//get weather information
+const getWeather = async (id) => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+  return data[0];
+};
+
+//set city information
+const getCity = async (city) => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+  return data[11];
+};
+
